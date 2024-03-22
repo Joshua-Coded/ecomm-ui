36a6bca2d923cc460bc1c651d0e8fffa9c9bdeb6
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 05:11:47 +0100

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="text-center p-2 border-t-2 bggray-200 border-gray-300 text-sm">
+                2024 @copy; by Joshua Alana.{" "}
+                <a href="https://github.com/PacktPublishing/Modern-API-Development-with-Spring-6-and-Spring-Boot-3">
+                    API-Development iwth Spring and Spring Boot
+                </a>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
