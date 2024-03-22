5a27d52273336be025495813426f8648af2783e7
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 05:32:15 +0100

    header ui done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col min-h-screen h-full">
+      <Header />
+      <Container />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@
 const Footer = () => {
     return (
         <div>
-            <footer className="text-center p-2 border-t-2 bggray-200 border-gray-300 text-sm">
-                2024 @copy; by Joshua Alana.{" "}
+            <footer className="text-center p-2 border-t-2 bg-gray-200 border-gray-300 text-sm">
+                2024 &copy; by Joshua Alana.{" "}
                 <a href="https://github.com/PacktPublishing/Modern-API-Development-with-Spring-6-and-Spring-Boot-3">
                     API-Development iwth Spring and Spring Boot
                 </a>
