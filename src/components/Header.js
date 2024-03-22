9f0fb3a39f8d7c614456300f9c230d82a37a5425
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 05:07:17 +0100

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <header className="p-2 border-b-2 border-gray-300 bggray-200">
+                <h1 className="text-lg font-bold">
+                    Ecommerce App
+                </h1>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
