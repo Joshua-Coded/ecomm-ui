d3183178a71e26b048aafbc4aae4a51a7a02a877
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 05:14:04 +0100

    the container demo

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+const Container = () => {
+    return (
+        <div>
+            <div className="flex-grow flex-shrink-0 p-4">
+                <p>Hello here, just the container</p>
+            </div>
+        </div>
+    );
+};
+
+export default Container;
