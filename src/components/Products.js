1d73234e139332a63e54537f9e25bd22eefb74cb
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-22 05:33:27 +0100

    products components

---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+
+import ProductCard from "./ProductCard";
+
+const Products = ({ auth, productList }) => {
+    return (
+        <>
+            {productList.map((item) => (
+                <ProductCard key={item.id} product={item} auth={auth} />
+            ))}
+        </>
+    );
+};
+
+export default Products;
